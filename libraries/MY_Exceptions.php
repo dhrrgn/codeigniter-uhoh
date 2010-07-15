22834d68ed021995d7217e8ed60c47021e1e8964
Dan Horrigan <dan@dhorrigan.com>
2010-07-15 08:41:31 -0400

    Missed a couple things on my last fix.  Oops.

--- libraries/MY_Exceptions.php
+++ libraries/MY_Exceptions.php
@@ -84,11 +84,11 @@ class MY_Exceptions extends CI_Exceptions
 	{
 		if (strpos($file, ABS_APPPATH) === 0)
 		{
-			$file = 'APPPATH/'.substr($file, strlen(realpath(FCPATH . APPPATH) . '/'));
+			$file = 'APPPATH/'.substr($file, strlen(ABS_APPPATH);
 		}
 		elseif (strpos($file, ABS_SYSDIR) === 0)
 		{
-			$file = 'SYSDIR/'.substr($file, strlen(BASEPATH));
+			$file = 'SYSDIR/'.substr($file, strlen(ABS_SYSDIR));
 		}
 		elseif (strpos($file, FCPATH) === 0)
 		{
