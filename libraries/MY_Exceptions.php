3994a6e6aec36669ce3e28adaf71f70b40afd5d6
Dan Horrigan <dan@dhorrigan.com>
2010-07-15 09:37:02 -0400

    Added a nicer looking error_general.php, also extended show_error so that it now doesn't send headers if they are already sent.

--- libraries/MY_Exceptions.php
+++ libraries/MY_Exceptions.php
@@ -432,6 +432,38 @@ class MY_Exceptions extends CI_Exceptions
 		return $output;
 	}
 
+	/**
+	 * General Error Page
+	 *
+	 * This function takes an error message as input
+	 * (either as a string or an array) and displays
+	 * it using the specified template.
+	 *
+	 * @access	private
+	 * @param	string	the heading
+	 * @param	string	the message
+	 * @param	string	the template name
+	 * @return	string
+	 */
+	function show_error($heading, $message, $template = 'error_general', $status_code = 500)
+	{
+		if(!headers_sent())
+		{
+			set_status_header($status_code);
+		}
+		
+		$message = '<p>'.implode('</p><p>', ( ! is_array($message)) ? array($message) : $message).'</p>';
+
+		if (ob_get_level() > $this->ob_level + 1)
+		{
+			ob_end_flush();	
+		}
+		ob_start();
+		include(APPPATH.'errors/'.$template.EXT);
+		$buffer = ob_get_contents();
+		ob_end_clean();
+		return $buffer;
+	}
 }
 
 /* End of file: MY_Exceptions.php */
